5c34d7d6080e9830f9b743f014288997d6982e64
Tammy Painter <126213362+teeleefy@users.noreply.github.com>
2024-08-02 08:01:15 -0500

    changed RESTful API endpoint

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3001";
-//const BASE_URL = process.env.VITE_APP_BASE_URL || "http://localhost:3001";
+// const BASE_URL = "http://localhost:3001";
+const BASE_URL = process.env.VITE_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
