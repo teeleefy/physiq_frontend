31e7c3b44266c8a09d75234ddc65a9c7d3f4b337
Tammy Painter <126213362+teeleefy@users.noreply.github.com>
2024-08-02 08:14:23 -0500

    edited api endpoint

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:3001";
 // const BASE_URL = process.env.VITE_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = process.env.VITE_APP_BASE_URL;
+// const BASE_URL = process.env.VITE_APP_BASE_URL;
+const BASE_URL = "https://physiq-backend.onrender.com";
 
 /** API Class.
  *
